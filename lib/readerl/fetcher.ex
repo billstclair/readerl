e4a314cedd7fe4e5bf0ea7f4bbd34022bc53150e
Bill St. Clair <billstclair@gmail.com>
2015-10-31 10:10:58 -0400

    Duh. Pass the URL arg to HTTPoison.get.

--- lib/readerl/fetcher.ex
+++ lib/readerl/fetcher.ex
@@ -4,7 +4,7 @@ defmodule Readerl.Fetcher do
     # I don't like ignoring SSL certificate signing, but even my own
     # web site's CA isn't in Hackney's list.
     {:ok, %HTTPoison.Response{body: body}} =
-      HTTPoison.get("https://lisplog.org/rss.xml", [], hackney: [:insecure])
+      HTTPoison.get(url, [], hackney: [:insecure])
     {:ok, feed, _} = FeederEx.parse(body)
     feed
   end
